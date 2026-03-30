243b9642adcb62a5dffda924421ab9f3eb6edc1a
Jsanson1 <161750793+Jsanson1@users.noreply.github.com>
2026-03-30 17:16:50 -0600

    typos

--- analysis/plot_diagnostics.py
+++ analysis/plot_diagnostics.py
@@ -4,8 +4,15 @@ import arviz as az
 import matplotlib.pyplot as plt
 
 idata = az.from_netcdf("analysis/lambda_model_fixed_nuts_quick.nc")
-az.plot_trace(idata, var_names=["delta_7","delta_1"])
-plt.show()
 
-az.plot_posterior(idata, var_names=["delta_7","delta_1"])
-plt.show()
+ax = az.plot_posterior(idata, var_names=["delta_7","delta_1"])
+plt.tight_layout()
+plt.savefig("analysis/posterior_deltas.png", dpi=200)
+plt.close()
+
+ax = az.plot_trace(idata, var_names=["delta_7","delta_1"])
+plt.tight_layout()
+plt.savefig("analysis/trace_deltas.png", dpi=200)
+plt.close()
+
+print("[OK] wrote analysis/posterior_deltas.png and analysis/trace_deltas.png")
